07c63a6d9bfe9b5b51b9bfa280c06cd131d5d645
Cheolkie Kim <ckiekim@naver.com>
2023-12-05 09:35:14 +0900

    first commit

---
+++ src/main/java/com/human/thymeleaf/ThymeleatApplication.java
@@ -0,0 +1,13 @@
+package com.human.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/human/thymeleaf/ThymeleatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
